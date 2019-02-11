822d52fb003020c23b84c955bdb8c2b2aafaee39
joao.victor <joao.victor@lojainterativa.com>
2019-02-11 18:33:33 -0200

    Alocar inicio

--- src/app/projetos/alocar-recurso-material-form/alocar-recurso-material-form.component.ts
+++ src/app/projetos/alocar-recurso-material-form/alocar-recurso-material-form.component.ts
@@ -1,13 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { ProjetosService } from '@app/projetos/projetos.service';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { RecursoMaterial, Projeto } from '@app/models';
 
 @Component({
     selector: 'app-alocar-recurso-material-form',
     templateUrl: './alocar-recurso-material-form.component.html',
     styleUrls: ['./alocar-recurso-material-form.component.scss']
 })
-export class AlocarRecursoMaterialFormComponent {
+export class AlocarRecursoMaterialFormComponent implements OnInit {
+
+    recursosMaterias: Array<any>;
+    empresaRecebdora: Array<any>;
+    projeto: Projeto;
 
     constructor(public activeModal: NgbActiveModal, private projetoService: ProjetosService) { }
+
+    ngOnInit() {
+        this.loadRecursoMaterial();
+        this.loadEmpresaRecebdora();
+        console.log(this.empresaRecebdora);
+    }
+
+    loadRecursoMaterial() {
+        this.projetoService.getRecursoMaterial(this.projeto.id).subscribe(recursosMaterias => this.recursosMaterias = recursosMaterias || []);
+    }
+    loadEmpresaRecebdora() {
+        this.projetoService.getEmpresa(this.projeto.id).subscribe(empresaRecebdora => {
+            console.log(empresaRecebdora);
+
+            this.empresaRecebdora = empresaRecebdora || [];
+        });
+    }
 }
--- src/app/projetos/projeto/recursos-materiais/alocacao.component.ts
+++ src/app/projetos/projeto/recursos-materiais/alocacao.component.ts
@@ -2,19 +2,50 @@ import { Component, OnInit } from '@angular/core';
 import { AlocarRecursoMaterialFormComponent } from '@app/projetos/alocar-recurso-material-form/alocar-recurso-material-form.component';
 import { ProjetosService } from '@app/projetos/projetos.service';
 import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs/operators';
+import { Projeto } from '@app/models';
+import { zip } from 'rxjs';
 
 @Component({
     selector: 'app-alocacao',
     templateUrl: './alocacao.component.html',
     styleUrls: ['./alocacao.component.scss']
 })
-export class AlocacaoComponent {
+export class AlocacaoComponent implements OnInit {
 
-    constructor(protected projetoService: ProjetosService, protected modalService: NgbModal) { }
+    alocacoes: Array<any>;
+    projeto: Projeto;
+
+    constructor(
+        private route: ActivatedRoute,
+        protected projetoService: ProjetosService,
+        protected modalService: NgbModal) { }
+
+    ngOnInit() {
+        const projeto$ = this.route.parent.data.pipe(map(d => d.projeto));
+        zip(projeto$).subscribe(([projeto]) => {
+            this.projeto = projeto;
+            this.loadAlocacao();
+        });
+    }
+
+    loadAlocacao() {
+        this.projetoService.getAlocacaoRM(this.projeto.id).subscribe(alocacoes => this.alocacoes = alocacoes || []);
+    }
 
     openModal(etapa_id: number) {
         const modalRef = this.modalService.open(AlocarRecursoMaterialFormComponent, { size: 'lg' });
-        modalRef.componentInstance.etapa_id = etapa_id;
+        //modalRef.componentInstance.etapa_id = etapa_id;
+        modalRef.componentInstance.projeto = this.projeto;
+
+        modalRef.result.then(result => {
+            this.loadAlocacao();
+
+        }, e => {
+
+        });
+
     }
 
 }
