d6b1874c1b07d30d61a1a572262c5420822574cd
Diego França <diego.franca@lojainterativa.com>
2019-02-12 12:26:46 -0200

    Implementada a restrição de subtemas repetidos

--- src/app/projetos/projeto/temas/sub-tema.component.ts
+++ src/app/projetos/projeto/temas/sub-tema.component.ts
@@ -3,50 +3,55 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { SubTema, CatalogSubTema } from '@app/models';
 
 @Component({
-  selector: 'app-sub-tema',
-  templateUrl: './sub-tema.component.html',
-  styles: [],
+    selector: 'app-sub-tema',
+    templateUrl: './sub-tema.component.html',
+    styles: [],
 })
 export class SubTemasComponent implements OnInit {
 
 
-  @Input() subTemas: CatalogSubTema[] = [];
-  @Input() subform: FormGroup;
-  @Input() index: number;
-  @Output() remove: EventEmitter<void> = new EventEmitter();
+    @Input() subTemas: CatalogSubTema[] = [];
+    @Input() selectedThemes: Array<number> = [];
+    @Input() subform: FormGroup;
+    @Input() index: number;
+    @Output() remove: EventEmitter<void> = new EventEmitter();
 
 
-
-  get temaControl() {
-    return this.subform.get('catalogSubTemaId');
-  }
-  get tema() {
-    return this.subTemas ? this.subTemas.find(t => t.subTemaId === parseInt(this.temaControl.value, 10)) : null;
-  }
-
-  get isOther() {
-    return this.tema && this.tema.nome.match(/^Outro\.?$/g) !== null;
-  }
-  constructor() { }
-
-  ngOnInit(): void {
-    // this.setup();
-
-    this.temaControl.valueChanges.subscribe(value => {
-      this.setup();
-    });
-  }
-
-  protected setup() {
-    if (this.isOther) {
-      this.subform.addControl('outroDesc', new FormControl('', [Validators.required]));
-    } else {
-      this.subform.removeControl('outroDesc');
+    get temas() {
+        return this.subTemas.filter(tema => {
+            return this.selectedThemes.indexOf(tema.subTemaId) === -1 || (this.tema && this.tema.subTemaId === tema.subTemaId);
+        });
     }
-    this.subform.updateValueAndValidity();
-  }
+    get temaControl() {
+        return this.subform.get('catalogSubTemaId');
+    }
+    get tema() {
+        return this.subTemas ? this.subTemas.find(t => t.subTemaId === parseInt(this.temaControl.value, 10)) : null;
+    }
 
-  delete() {
-    this.remove.emit();
-  }
+    get isOther() {
+        return this.tema && this.tema.nome.match(/^Outro\.?$/g) !== null;
+    }
+    constructor() { }
+
+    ngOnInit(): void {
+        // this.setup();
+
+        this.temaControl.valueChanges.subscribe(value => {
+            this.setup();
+        });
+    }
+
+    protected setup() {
+        if (this.isOther) {
+            this.subform.addControl('outroDesc', new FormControl('', [Validators.required]));
+        } else {
+            this.subform.removeControl('outroDesc');
+        }
+        this.subform.updateValueAndValidity();
+    }
+
+    delete() {
+        this.remove.emit();
+    }
 }
--- src/app/projetos/projeto/temas/temas.component.ts
+++ src/app/projetos/projeto/temas/temas.component.ts
@@ -11,126 +11,130 @@ import { SubTemasComponent } from './sub-tema.component';
 import { LoadingComponent } from '@app/shared/loading/loading.component';
 
 @Component({
-  selector: 'app-temas',
-  templateUrl: './temas.component.html',
-  styleUrls: ['./temas.component.scss']
+    selector: 'app-temas',
+    templateUrl: './temas.component.html',
+    styleUrls: ['./temas.component.scss']
 })
 export class TemasComponent implements OnInit {
 
-  projeto: Projeto;
-  temaProjeto: TemaProjeto;
+    projeto: Projeto;
+    temaProjeto: TemaProjeto;
 
-  temas: Array<Tema>;
-  // Form
-  form: FormGroup;
-  temaControl = new FormControl('', [Validators.required]);
-  subTemasForms = new FormArray([]);
+    temas: Array<Tema>;
+    // Form
+    form: FormGroup;
+    temaControl = new FormControl('', [Validators.required]);
+    subTemasForms = new FormArray([]);
 
-  @ViewChild(SubTemasComponent) subTemasComponent;
-  @ViewChild(LoadingComponent) loading;
+    @ViewChild(SubTemasComponent) subTemasComponent;
+    @ViewChild(LoadingComponent) loading;
 
-  get tema() {
-    return this.temas ? this.temas.find(t => t.id === parseInt(this.temaControl.value, 10)) : null;
-  }
-
-  get subTemas() {
-    return this.tema ? this.tema.subTemas : [];
-  }
-
-  constructor(
-    private app: AppService,
-    private route: ActivatedRoute,
-    private projetoService: ProjetosService,
-    protected catalogo: CatalogsService) {
-  }
-
-  ngOnInit() {
-    this.loading.show();
-    const temas$ = this.catalogo.temas();
-    const projeto$ = this.route.parent.data.pipe(map(d => d.projeto));
-
-    zip(temas$, projeto$).subscribe(([temas, projeto]) => {
-      this.temas = temas;
-      this.projeto = projeto;
-      this.projetoService.getTema(projeto.id).subscribe(temaProjeto => {
-        this.temaProjeto = temaProjeto;
-        this.setupForm(this.projeto, temaProjeto);
-        this.loading.hide();
-      });
-    });
-  }
-
-  protected setupForm(projeto: Projeto, temas: TemaProjeto = null) {
-
-    this.form = new FormGroup({
-      projetoId: new FormControl(projeto.id, [Validators.required]),
-      catalogTemaId: this.temaControl,
-      outroDesc: new FormControl(temas ? (temas.outroDesc || '') : ''),
-      subTemas: this.subTemasForms
-    });
-
-    this.temaControl.valueChanges.subscribe(value => {
-      if (this.tema.valor === 'OU') {
-        this.form.addControl('outroDesc', new FormControl('', [Validators.required]));
-      } else {
-        this.form.removeControl('outroDesc');
-      }
-      this.reset();
-    });
-
-
-
-    if (temas) {
-      this.form.addControl('Id', new FormControl(temas.id));
-      this.form.get('catalogTemaId').setValue(temas.catalogTemaId);
-      this.reset(false);
-      temas.subTemas.forEach(s => {
-        this.addSubTema(s);
-      });
+    get tema() {
+        return this.temas ? this.temas.find(t => t.id === parseInt(this.temaControl.value, 10)) : null;
     }
-  }
 
-  protected reset(keepOne = true) {
-    // this.subTemasForms.reset();
-    while (this.subTemasForms.length > 0) {
-      this.subTemasForms.removeAt(0);
+    get subTemas() {
+        return this.tema ? this.tema.subTemas : [];
     }
-    if (this.tema && keepOne) {
-      this.addSubTema();
+
+    get selectedsThemes() {
+        return (this.subTemasForms.value as Array<any>).map(i => parseInt(i.catalogSubTemaId, 10));
     }
-  }
 
-
-  addSubTema(subtema?: SubTema) {
-
-    const id = subtema ? subtema.catalogSubTemaId || '' : '';
-    const outroDesc = subtema ? subtema.outroDesc || '' : '';
-
-    const f = new FormGroup({
-      catalogSubTemaId: new FormControl(id, [Validators.required]),
-      outroDesc: new FormControl(outroDesc)
-    });
-    this.subTemasForms.push(f);
-  }
-  delete(i: number) {
-    if (this.subTemasForms.length > 1) {
-      this.subTemasForms.removeAt(i);
+    constructor(
+        private app: AppService,
+        private route: ActivatedRoute,
+        private projetoService: ProjetosService,
+        protected catalogo: CatalogsService) {
     }
-  }
 
-  updateTemas() {
-    this.loading.show();
-    const request = this.temaProjeto ? this.projetoService.editTema(this.form.value) : this.projetoService.criarTema(this.form.value);
+    ngOnInit() {
+        this.loading.show();
+        const temas$ = this.catalogo.temas();
+        const projeto$ = this.route.parent.data.pipe(map(d => d.projeto));
 
-    request.subscribe(resultado => {
-      this.loading.hide();
-      if (resultado.sucesso) {
-        this.app.alert("Tema atualizado com sucesso");
-      } else {
-        this.app.alert(resultado.inconsistencias.join(", "));
-      }
-    });
+        zip(temas$, projeto$).subscribe(([temas, projeto]) => {
+            this.temas = temas;
+            this.projeto = projeto;
+            this.projetoService.getTema(projeto.id).subscribe(temaProjeto => {
+                this.temaProjeto = temaProjeto;
+                this.setupForm(this.projeto, temaProjeto);
+                this.loading.hide();
+            });
+        });
+    }
 
-  }
+    protected setupForm(projeto: Projeto, temas: TemaProjeto = null) {
+
+        this.form = new FormGroup({
+            projetoId: new FormControl(projeto.id, [Validators.required]),
+            catalogTemaId: this.temaControl,
+            outroDesc: new FormControl(temas ? (temas.outroDesc || '') : ''),
+            subTemas: this.subTemasForms
+        });
+
+        this.temaControl.valueChanges.subscribe(value => {
+            if (this.tema.valor === 'OU') {
+                this.form.addControl('outroDesc', new FormControl('', [Validators.required]));
+            } else {
+                this.form.removeControl('outroDesc');
+            }
+            this.reset();
+        });
+
+
+
+        if (temas) {
+            this.form.addControl('Id', new FormControl(temas.id));
+            this.form.get('catalogTemaId').setValue(temas.catalogTemaId);
+            this.reset(false);
+            temas.subTemas.forEach(s => {
+                this.addSubTema(s);
+            });
+        }
+    }
+
+    protected reset(keepOne = true) {
+        // this.subTemasForms.reset();
+        while (this.subTemasForms.length > 0) {
+            this.subTemasForms.removeAt(0);
+        }
+        if (this.tema && keepOne) {
+            this.addSubTema();
+        }
+    }
+
+
+    addSubTema(subtema?: SubTema) {
+
+        const id = subtema ? subtema.catalogSubTemaId || '' : '';
+        const outroDesc = subtema ? subtema.outroDesc || '' : '';
+
+        const f = new FormGroup({
+            catalogSubTemaId: new FormControl(id, [Validators.required]),
+            outroDesc: new FormControl(outroDesc)
+        });
+        this.subTemasForms.push(f);
+    }
+    delete(i: number) {
+        if (this.subTemasForms.length > 1) {
+            this.subTemasForms.removeAt(i);
+        }
+    }
+
+    updateTemas() {
+        this.loading.show();
+        const request = this.temaProjeto ? this.projetoService.editTema(this.form.value) : this.projetoService.criarTema(this.form.value);
+
+        request.subscribe(resultado => {
+            this.loading.hide();
+            if (resultado.sucesso) {
+                this.app.alert("Tema atualizado com sucesso");
+            } else {
+                this.app.alert(resultado.inconsistencias.join(", "));
+            }
+        });
+
+    }
 
 }
