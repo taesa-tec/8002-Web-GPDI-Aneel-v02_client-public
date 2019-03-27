aaf76b4d87416258f483759a3d62acb86202b303
Diego França <diego.franca@lojainterativa.com>
2019-03-27 10:28:22 -0300

    Otimização dos meus projetos

--- src/app/dashboard/meus-projetos/meus-projetos.component.ts
+++ src/app/dashboard/meus-projetos/meus-projetos.component.ts
@@ -9,56 +9,51 @@ import { CatalogsService } from '@app/catalogs/catalogs.service';
 import { LoadingComponent } from '@app/shared/loading/loading.component';
 import { of, merge, Observable, interval, zip } from 'rxjs';
 import { mapTo, delay } from 'rxjs/operators';
+import { AppService } from '@app/app.service';
 
 @Component({
-  selector: 'app-meus-projetos',
-  templateUrl: './meus-projetos.component.html',
-  styleUrls: ['./meus-projetos.component.scss']
+    selector: 'app-meus-projetos',
+    templateUrl: './meus-projetos.component.html',
+    styleUrls: []
 })
 export class MeusProjetosComponent implements OnInit {
 
-  projetos: Projeto[];
+    projetos: Projeto[];
 
-  @ViewChild(LoadingComponent) loading: LoadingComponent;
+    @ViewChild(LoadingComponent) loading: LoadingComponent;
 
-  constructor(
-    protected catalogs: CatalogsService,
-    protected projetoService: ProjetosService,
-    protected modalService: NgbModal) {
-    this.projetos = [];
-  }
+    constructor(protected app: AppService) {
+        this.projetos = [];
+    }
 
-  openNovoProjeto() {
-    this.modalService.open(NovoProjetoComponent, { size: 'lg' }).result.then(value => {
-      if (value.sucesso) {
-        this.loadData();
-      }
-    }).catch(reason => {
-
-    });
-
-  }
-
-  ngOnInit() {
-    this.loadData();
-  }
-  loadData() {
-    this.loading.show();
-    zip(this.getProjetos()).subscribe(() => {
-      this.loading.hide();
-    });
-  }
-
-  getProjetos() {
-    return new Observable((ob) => {
-      this.projetoService.meusProjetos()
-        // this.projetoService.getProjetos()
-        .subscribe(p$ => {
-          this.projetos = p$.map(p$ => p$.projeto);
-          ob.next();
+    openNovoProjeto() {
+        this.app.modal.open(NovoProjetoComponent, { size: 'lg' }).result.then(value => {
+            if (value.sucesso) {
+                this.loadData();
+            }
+        }).catch(reason => {
+            console.log(reason);
         });
-    });
-  }
 
+    }
+
+    ngOnInit() {
+        this.app.users.currentUserUpdated.subscribe(user => {
+            this.loadData();
+        });
+    }
+    loadData() {
+        this.loading.show();
+        zip(this.app.projetos.meusProjetos()).subscribe(([projetos]) => {
+            this.projetos = projetos.map(p$ => p$.projeto);
+            this.projetos.forEach(projeto => {
+                this.app.users.currentUserCanAccess(projeto).subscribe(can => {
+                    console.log({ projeto, can });
+
+                });
+            });
+            this.loading.hide();
+        });
+    }
 
 }
