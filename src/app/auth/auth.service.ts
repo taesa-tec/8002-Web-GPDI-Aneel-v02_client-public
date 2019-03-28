d6e22b1c48359c329b51e1dbabb94ccb97b05a3a
Diego França <diego.franca@lojainterativa.com>
2019-03-28 16:13:09 -0300

    Fix bug de requisições repetidas

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,103 +1,100 @@
 import { Injectable, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { APP_CONFIG, AppConfig } from '../app.config';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { LoginRequest, RecoverRequest, ResultadoResponse, NewpassRequest } from '@app/models';
 import { LoginResponse } from '@app/models';
 import { Router } from '@angular/router';
 
 const storageKey = 'loggedUser';
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthService {
 
 
-  public redirectTo = "/dashboard";
-  private loginResponse: LoginResponse;
+    public redirectTo = "/dashboard";
+    private loginResponse: LoginResponse;
 
-  constructor(private http: HttpClient, @Inject(APP_CONFIG) config: AppConfig, protected router: Router) {
+    protected authEventsSource = new BehaviorSubject<{ type: string, data?: any }>(null);
 
-    const loggedUser = localStorage.getItem(storageKey);
-    if (loggedUser) {
-      try {
-        this.loginResponse = JSON.parse(loggedUser);
-      } catch (e) {
-        console.log(e.message);
+    authEvent = this.authEventsSource.asObservable();
 
-      }
-    }
-  }
+    constructor(private http: HttpClient, protected router: Router) {
 
-  get expiration() {
-    if (this.loginResponse && this.loginResponse.expiration) {
-      return new Date(this.loginResponse.expiration);
-    }
-    return null;
-  }
-
-  get token() {
-    if (this.loginResponse && this.loginResponse.authenticated) {
-      return this.loginResponse.accessToken;
-    }
-    return false;
-  }
-
-  get isLoggedIn() {
-    if (this.token && this.expiration !== null && this.expiration.getTime() > Date.now()) {
-      return true;
-    }
-
-    return false;
-  }
-
-  login(loginRequest: LoginRequest, remember: boolean = false, redirectTo: string | null = null): Observable<LoginResponse> {
-    return new Observable<LoginResponse>(rootObserver => {
-
-      const self = this;
-
-      if (redirectTo) {
-        this.redirectTo = redirectTo;
-      }
-
-      const o = this.http.post<LoginResponse>(`Login`, loginRequest);
-
-      o.subscribe({
-        next(loginResponse) {
-          if (loginResponse.authenticated) {
-            self.loginResponse = loginResponse;
-            if (remember) {
-              localStorage.setItem(storageKey, JSON.stringify(loginResponse));
+        const loggedUser = localStorage.getItem(storageKey);
+        if (loggedUser) {
+            try {
+                this.loginResponse = JSON.parse(loggedUser);
+            } catch (e) {
+                console.log(e.message);
             }
-            self.router.navigateByUrl(self.redirectTo);
-          }
-          rootObserver.next(loginResponse);
-        },
-        error(e) {
-          rootObserver.error(e);
-        },
-        complete() {
-          rootObserver.complete();
         }
-      });
-    });
-    // this.http.post(`${this.api}/login`, user);
-  }
+    }
 
-  logout(): void {
-    this.loginResponse = null;
-    localStorage.removeItem(storageKey);
-    this.redirectTo = this.router.url;
-    this.router.navigate(['/login']);
-  }
+    get expiration() {
+        if (this.loginResponse && this.loginResponse.expiration) {
+            return new Date(this.loginResponse.expiration);
+        }
+        return null;
+    }
 
-  recuperarSenha(recoverRequest: RecoverRequest): Observable<ResultadoResponse> {
-    return this.http.post<ResultadoResponse>('Login/recuperar-senha', recoverRequest);
-  }
+    get token() {
+        if (this.loginResponse && this.loginResponse.authenticated) {
+            return this.loginResponse.accessToken;
+        }
+        return false;
+    }
 
-  novaSenha(recoverRequest: NewpassRequest): Observable<ResultadoResponse> {
-    return this.http.post<ResultadoResponse>('Login/nova-senha', recoverRequest);
-  }
+    get isLoggedIn() {
+        if (this.token && this.expiration !== null && this.expiration.getTime() > Date.now()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    login(loginRequest: LoginRequest, remember: boolean = false, redirectTo: string | null = null): Observable<LoginResponse> {
+        return new Observable<LoginResponse>(rootObserver => {
+            if (redirectTo) {
+                this.redirectTo = redirectTo;
+            }
+
+            this.http.post<LoginResponse>(`Login`, loginRequest).subscribe(loginResponse => {
+                if (loginResponse.authenticated) {
+
+                    this.loginResponse = loginResponse;
+                    if (remember) {
+                        localStorage.setItem(storageKey, JSON.stringify(loginResponse));
+                    }
+                    this.router.navigateByUrl(this.redirectTo);
+                    this.authEventsSource.next({ type: 'login', data: loginResponse });
+                }
+                rootObserver.next(loginResponse);
+            }, error => {
+                rootObserver.error(error);
+            }, () => {
+                rootObserver.complete();
+            });
+        });
+        // this.http.post(`${this.api}/login`, user);
+    }
+
+    logout(): void {
+        this.loginResponse = null;
+        localStorage.removeItem(storageKey);
+        this.redirectTo = this.router.url;
+        this.router.navigate(['/login']);
+        this.authEventsSource.next({ type: 'logout' });
+    }
+
+    recuperarSenha(recoverRequest: RecoverRequest): Observable<ResultadoResponse> {
+        return this.http.post<ResultadoResponse>('Login/recuperar-senha', recoverRequest);
+    }
+
+    novaSenha(recoverRequest: NewpassRequest): Observable<ResultadoResponse> {
+        return this.http.post<ResultadoResponse>('Login/nova-senha', recoverRequest);
+    }
 
 
 
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -10,49 +10,42 @@ import { CatalogsService } from '@app/catalogs/catalogs.service';
 import { User } from '@app/models';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
 
-  @ViewChild(LoadingComponent)
-  private loading: LoadingComponent;
+    @ViewChild(LoadingComponent)
+    private loading: LoadingComponent;
 
-  currentUser: User;
+    currentUser: User;
 
-  constructor(
-    protected users: UsersService,
-    protected catalogs: CatalogsService,
-    protected uiService: AppService,
-    protected projetos: ProjetosService,
-    protected auth: AuthService
-  ) { }
+    constructor(
+        protected app: AppService,
+    ) { }
 
-  get avatar() {
-    return (this.currentUser && this.currentUser.fotoPerfil) ?
-      `data:image/jpeg;base64,${this.currentUser.fotoPerfil.file}` : '/assets/default_avatar.png';
-  }
-  get empresa() {
-    if (this.currentUser) {
-      return this.currentUser.catalogEmpresa ?
-        this.currentUser.catalogEmpresa.nome :
-        (this.currentUser.razaoSocial ? this.currentUser.razaoSocial : '')
+    get avatar() {
+        return (this.currentUser && this.currentUser.fotoPerfil) ?
+            `data:image/jpeg;base64,${this.currentUser.fotoPerfil.file}` : '/assets/default_avatar.png';
     }
-    return '';
-  }
+    get empresa() {
+        if (this.currentUser) {
+            return this.currentUser.catalogEmpresa ?
+                this.currentUser.catalogEmpresa.nome :
+                (this.currentUser.razaoSocial ? this.currentUser.razaoSocial : '')
+        }
+        return '';
+    }
 
-  logout() {
-    this.auth.logout();
-  }
+    logout() {
+        this.app.auth.logout();
+    }
 
-  ngOnInit() {
-    this.users.me().subscribe(currentUser => {
-      this.currentUser = currentUser;
-    });
-    this.users.currentUserUpdated.subscribe(user => {
-      this.currentUser = user;
-    });
-  }
+    ngOnInit() {
+        this.app.users.currentUserUpdated.subscribe(user => {
+            this.currentUser = user;
+        });
+    }
 
 }
