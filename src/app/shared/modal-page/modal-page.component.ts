8e27abc0916b1c5fa6e5ba1a852e106c0c80ceb4
Diego França <diego.franca@lojainterativa.com>
2019-04-11 17:35:48 -0300

    Correções de cores modal page
    Captura da tag title para o titulo do modal

--- src/app/shared/modal-page/modal-page.component.ts
+++ src/app/shared/modal-page/modal-page.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 import {SafeHtml} from '@angular/platform-browser';
 import htmlpages from '@htmlpages/index';
@@ -6,22 +6,40 @@ import htmlpages from '@htmlpages/index';
 @Component({
     selector: 'app-modal-page',
     templateUrl: './modal-page.component.html',
-    styleUrls: ['./modal-page.component.scss']
+    styleUrls: ['./modal-page.component.scss'],
+    encapsulation: ViewEncapsulation.None
 })
 export class ModalPageComponent implements OnInit {
 
+    parser = new DOMParser();
     protected htmlContent: SafeHtml;
+
+    protected _title = 'Detalhamento';
+    get title() {
+        return this._title;
+    }
+
+    set title(title: string) {
+        this._title = title.length > 0 ? title : 'Detalhamento';
+    }
 
     constructor(public activeModal: NgbActiveModal) {
     }
 
     setPage(page: string) {
         if (htmlpages[page]) {
-            this.htmlContent = htmlpages[page];
+            try {
+                this.htmlContent = htmlpages[page];
+                const parsed = this.parser.parseFromString(this.htmlContent.toString(), 'text/html');
+                this.title = parsed.head.innerText.trim();
+            } catch (e) {
+                console.log(e);
+            }
         } else {
             throw new Error('Página não encontrada');
         }
     }
+
 
     ngOnInit() {
     }
