769734fa018a64f814d1d6fe35914bb0e8e53452
Diego França <diego.franca@lojainterativa.com>
2019-11-11 11:50:32 -0300

    Fix Download pdf das demandas

--- src/app/dashboard/shared/pdf-viewer/pdf-viewer.component.ts
+++ src/app/dashboard/shared/pdf-viewer/pdf-viewer.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpEvent, HttpEventType} from '@angular/common/http';
 
 @Component({
   selector: 'app-pdf-viewer',
@@ -10,23 +10,39 @@ import {HttpClient} from '@angular/common/http';
 export class PdfViewerComponent implements OnInit {
 
   @Input() url: string;
+  protected realUrlFile: string;
+  filename: string;
   loadedFileUrl: SafeResourceUrl;
 
   constructor(protected http: HttpClient, protected sanitizer: DomSanitizer) {
   }
 
+
   async ngOnInit() {
     if (this.url) {
       try {
 
-        const file = await this.http.get(this.url, {
-          responseType: 'blob'
-        }).toPromise();
-        this.loadedFileUrl = this.sanitizer.bypassSecurityTrustResourceUrl(URL.createObjectURL(file));
+        const response = await this.http.get(this.url, {observe: 'response', responseType: 'blob'}).toPromise();
+        const content = response.headers.get('content-disposition');
+        if (content) {
+          this.filename = content.split(';')
+            .map(i => i.trim())
+            .find(i => i.startsWith('filename'))
+            .replace(/^filename=/, '');
+        }
+        this.realUrlFile = URL.createObjectURL(response.body);
+        this.loadedFileUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.realUrlFile);
       } catch (e) {
         console.error(e.message);
       }
     }
+  }
+
+  download() {
+    const a = document.createElement('a');
+    a.href = this.realUrlFile;
+    a.setAttribute('download', this.filename);
+    a.click();
   }
 
 }
