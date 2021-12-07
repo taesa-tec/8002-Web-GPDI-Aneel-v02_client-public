bf78948b396eb3bb5e4e85964d76edc7c04011c5
Diego França <diego.franca@lojainterativa.com>
2021-12-07 11:15:33 -0300

    Códigos de alta complexidade

--- src/app/pages/propostas/proposta/shared/providers.ts
+++ src/app/pages/propostas/proposta/shared/providers.ts
@@ -9,6 +9,7 @@ import {PropostaTexts} from '@app/pages/propostas/proposta/shared/texts';
 import {TABLE_ACTIONS} from '@app/core/components';
 import {AuthService} from '@app/services';
 
+
 export const CaptacaoIdProvider: Provider = {
   provide: CAPTACAO_ID,
   deps: [ActivatedRoute],
@@ -35,23 +36,45 @@ export const PropostaProvider: Provider = {
   }
 };
 
+const MENU_ITENS: Array<MenuItem> = [
+  {path: 'detalhes', text: 'Detalhes da Demanda', icon: 'ta-search'},
+  {path: 'condicoes', text: 'Condições Fundamentais para Fornecimento', icon: 'ta-ficha'}
+];
+const MENU_ITENS_FULL: Array<MenuItem> = [
+  ...MENU_ITENS,
+  {path: 'entidades', text: 'Cadastro Outras Entidades', icon: 'ta-empresas'},
+  {path: 'plano-de-trabalho', text: 'Plano de Trabalho', icon: 'ta-work-plan'},
+  {path: 'produtos', text: 'Produtos', icon: 'ta-box'},
+  {path: 'etapas', text: 'Etapas', icon: 'ta-etapas'},
+  {path: 'escopo', text: 'Escopo', icon: 'ta-projeto'},
+  {path: 'riscos', text: 'Tabela de Riscos', icon: 'ta-alert'},
+  {path: 'recursos-humanos', text: 'Recursos Humanos', icon: 'ta-group'},
+  {path: 'alocacao-recursos-humanos', text: 'Alocação de Recursos Humanos', icon: 'ta-alocacao-rh'},
+  {path: 'recursos-materiais', text: 'Recursos Materiais', icon: 'ta-recurso-material'},
+  {path: 'alocacao-recursos-materiais', text: 'Alocação de Recursos Materiais', icon: 'ta-alocacao-material'},
+];
+const alertIcon = '<span class="badge badge-danger"><i class="align-middle ta-notification text-white"></i></span>';
+const awaitingIcon = '<span class="badge badge-warning"><i class="align-middle ta-ampulheta text-white"></i></span>';
+const getIcon = (status, isResponsavel) => {
+  switch (status) {
+    case 'Pendente':
+      return isResponsavel ? awaitingIcon : alertIcon;
+    case 'Alteracao':
+      return !isResponsavel ? awaitingIcon : alertIcon;
+  }
+  return null;
+};
+
 export const PropostaSidebar: Provider = {
   provide: SIDEBAR_MENU,
   deps: [PROPOSTA, AuthService],
   useFactory: (propostaObservable: BehaviorSubject<Proposta>, auth: AuthService) => {
-    const behavior = new BehaviorSubject<Array<MenuItem>>([
-      {path: 'detalhes', text: 'Detalhes da Demanda', icon: 'ta-search'},
-      {path: 'condicoes', text: 'Condições Fundamentais para Fornecimento', icon: 'ta-ficha'}
-    ]);
+    const behavior = new BehaviorSubject<Array<MenuItem>>(MENU_ITENS);
 
     propostaObservable.subscribe(proposta => {
       const isResponsavel = auth.getUser().id === proposta.responsavelId;
-      const alertIcon = '<span class="badge badge-danger"><i class="align-middle ta-notification text-white"></i></span>';
-      const awaitingIcon = '<span class="badge badge-warning"><i class="align-middle ta-ampulheta text-white"></i></span>';
 
-      let menu_itens: Array<MenuItem> = [
-        {path: 'detalhes', text: 'Detalhes da Demanda', icon: 'ta-search'},
-        {path: 'condicoes', text: 'Condições Fundamentais para Fornecimento', icon: 'ta-ficha'}];
+      let menu_itens: Array<MenuItem> = MENU_ITENS;
 
       if (proposta.dataClausulasAceitas !== null && (proposta.participacao === 1 || proposta.participacao === 3)) {
 
@@ -60,32 +83,13 @@ export const PropostaSidebar: Provider = {
         contratoAlert = proposta.captacaoStatus === 'Refinamento' && proposta.contratoAprovacao === 'Alteracao' ? alertIcon : '';
         planoAlert = proposta.captacaoStatus === 'Refinamento' && proposta.planoTrabalhoAprovacao === 'Alteracao' ? alertIcon : '';
 
-        const getIcon = s => {
-          switch (s) {
-            case 'Pendente':
-              return isResponsavel ? awaitingIcon : alertIcon;
-            case 'Alteracao':
-              return !isResponsavel ? awaitingIcon : alertIcon;
-          }
-          return null;
-        };
-
         if (proposta.captacaoStatus === 'Refinamento') {
-          contratoAlert = getIcon(proposta.contratoAprovacao);
-          planoAlert = getIcon(proposta.planoTrabalhoAprovacao);
+          contratoAlert = getIcon(proposta.contratoAprovacao, isResponsavel);
+          planoAlert = getIcon(proposta.planoTrabalhoAprovacao, isResponsavel);
         }
+
         menu_itens = [
-          ...menu_itens,
-          {path: 'entidades', text: 'Cadastro Outras Entidades', icon: 'ta-empresas'},
-          {path: 'plano-de-trabalho', text: 'Plano de Trabalho', icon: 'ta-work-plan'},
-          {path: 'produtos', text: 'Produtos', icon: 'ta-box'},
-          {path: 'etapas', text: 'Etapas', icon: 'ta-etapas'},
-          {path: 'escopo', text: 'Escopo', icon: 'ta-projeto'},
-          {path: 'riscos', text: 'Tabela de Riscos', icon: 'ta-alert'},
-          {path: 'recursos-humanos', text: 'Recursos Humanos', icon: 'ta-group'},
-          {path: 'alocacao-recursos-humanos', text: 'Alocação de Recursos Humanos', icon: 'ta-alocacao-rh'},
-          {path: 'recursos-materiais', text: 'Recursos Materiais', icon: 'ta-recurso-material'},
-          {path: 'alocacao-recursos-materiais', text: 'Alocação de Recursos Materiais', icon: 'ta-alocacao-material'},
+          ...MENU_ITENS_FULL,
           {path: 'contrato', text: `Validação Contrato`, icon: 'ta-gavel', afterText: contratoAlert},
           {path: 'envio', text: `Envio Proposta para aprovação`, icon: 'ta-ok', afterText: planoAlert},
         ];
